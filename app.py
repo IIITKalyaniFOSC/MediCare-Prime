a8a2bbea6f630bb5d2450e6f8a81c8708cb4b39b
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-06-02 15:04:19 +0530

    Adding Dengue model

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def model_predictKidney(values):
     model = pickle.load(open('models/kidney/kidney.pkl','rb'))
     values = np.asarray(values)
     return model.predict(values.reshape(1, -1))[0]
+
+def model_predictDengue(values):
+    model = pickle.load(open('models/dengue/Dengue.pkl','rb'))
+    values = np.asarray(values)
+    return model.predict(values.reshape(1, -1))[0]
     
 def model_predictMalaria(img_path):
     model = load_model('models/malaria/malaria.h5')
@@ -34,6 +39,10 @@ def home():
 def kidney():
     return render_template('kidney.html')
     
+@app.route("/dengue", methods=['GET', 'POST'])
+def dengue():
+    return render_template('dengue.html')
+
 @app.route("/malaria", methods=['GET', 'POST'])
 def malaria():
     return render_template('malaria.html')
@@ -50,6 +59,19 @@ def predictKidney():
         return render_template("index_content.html", message = message)
 
     return render_template('predictKidney.html', pred = pred)
+    
+@app.route("/predictDengue", methods = ['POST', 'GET'])
+def predictDengue():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = model_predictDengue(to_predict_list)
+    except:
+        message = "Please enter valid Data"
+        return render_template("index_content.html", message = message)
+
+    return render_template('predictDengue.html', pred = pred)
     
 @app.route("/predictMalaria", methods = ['POST', 'GET'])
 def predictMalaria():
